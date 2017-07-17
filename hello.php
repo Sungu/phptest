e34fa2e3ae2d965da111ebf8dc6d0f063a3822ec
Sungu Nam <naisz@naver.com>
2017-07-17 05:20:35 +0000

    etdt

--- hello.php
+++ hello.php
@@ -4,28 +4,49 @@ header("Content-Type:application/json");
 // 1. 데이터베이스에서 데이터를 가져옴
 $conn = pg_connect('host=heart5.kaist.ac.kr dbname=codatest_mjkwon port=5432 user=bisler password=bislaprom3 connect_timeout=5') or die('Could not connect: '.pg_last_error());
 
-$query = "SELECT * FROM gene where entrezid='6257';";
-$query .= "SELECT * FROM gene where entrezid='5468';";
-$query .= "SELECT * FROM gene where entrezid='7849';";
-$query .= "SELECT * FROM gene where entrezid='6256';";
+$query1 = "SELECT * FROM gene where entrezid='6257';";
+$query2 = "SELECT * FROM gene where entrezid='5468';";
+$query3 = "SELECT * FROM gene where entrezid='7849';";
+$query4 = "SELECT * FROM gene where entrezid='6256';";
 
 // $result = pg_query($conn, "SELECT * FROM gene where symbol= 'TP53'");
-$result = pg_query($conn, $query);
+$result1 = pg_query($conn, $query1);
+$result2 = pg_query($conn, $query2);
+$result3 = pg_query($conn, $query3);
+$result4 = pg_query($conn, $query4);
     // 2. 데이터베이스로부터 반환된 데이터를
     // 객체 형태로 가공함
-if (!$result){
-    echo "An error occurred.\n";
-    exit;
-}
 
-$data = array();
+$data1 = array();
 while ($row = pg_fetch_row($result)) {
     
-    $data[] = array('geneid'=> $row[0],'entrezid'=> $row[1],'a_ensemblid'=> $row[2],'a_biogridid'=> $row[3],'a_pharmgkbid'=> $row[4],'symbol'=> $row[5],
+    $data1[] = array('geneid'=> $row[0],'entrezid'=> $row[1],'a_ensemblid'=> $row[2],'a_biogridid'=> $row[3],'a_pharmgkbid'=> $row[4],'symbol'=> $row[5],
                     'a_synonym'=> $row[6],'ncbitaxid'=> $row[7],'a_uniprotid'=> $row[8],'a_ecnumber'=> $row[9],'a_goid'=> $row[10]);
 }
-echo json_encode($data);
-$fp = fopen('dic.json', 'w');
-fwrite($fp, json_encode($data));
-fclose($fp);
+
+$data2 = array();
+while ($row = pg_fetch_row($result)) {
+    
+    $data2[] = array('geneid'=> $row[0],'entrezid'=> $row[1],'a_ensemblid'=> $row[2],'a_biogridid'=> $row[3],'a_pharmgkbid'=> $row[4],'symbol'=> $row[5],
+                    'a_synonym'=> $row[6],'ncbitaxid'=> $row[7],'a_uniprotid'=> $row[8],'a_ecnumber'=> $row[9],'a_goid'=> $row[10]);
+}
+
+$data3 = array();
+while ($row = pg_fetch_row($result)) {
+    
+    $data3[] = array('geneid'=> $row[0],'entrezid'=> $row[1],'a_ensemblid'=> $row[2],'a_biogridid'=> $row[3],'a_pharmgkbid'=> $row[4],'symbol'=> $row[5],
+                    'a_synonym'=> $row[6],'ncbitaxid'=> $row[7],'a_uniprotid'=> $row[8],'a_ecnumber'=> $row[9],'a_goid'=> $row[10]);
+}
+
+$data4 = array();
+while ($row = pg_fetch_row($result)) {
+    
+    $data4[] = array('geneid'=> $row[0],'entrezid'=> $row[1],'a_ensemblid'=> $row[2],'a_biogridid'=> $row[3],'a_pharmgkbid'=> $row[4],'symbol'=> $row[5],
+                    'a_synonym'=> $row[6],'ncbitaxid'=> $row[7],'a_uniprotid'=> $row[8],'a_ecnumber'=> $row[9],'a_goid'=> $row[10]);
+}
+
+echo $data1;
+// $fp = fopen('dic.json', 'w');
+// fwrite($fp, json_encode($data));
+// fclose($fp);
 
