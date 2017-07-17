24fb7b3057eee0fdf8632bfaa0fc7b7c9ffae297
Sungu Nam <naisz@naver.com>
2017-07-17 04:14:12 +0000

    pelase

--- hello.php
+++ hello.php
@@ -4,12 +4,7 @@ header("Content-Type:application/json");
 // 1. 데이터베이스에서 데이터를 가져옴
 $conn = pg_connect('host=heart5.kaist.ac.kr dbname=codatest_mjkwon port=5432 user=bisler password=bislaprom3 connect_timeout=5') or die('Could not connect: '.pg_last_error());
 
-if (!$conn) {
-  echo "An error occurred.\n";
-  exit;
-}
-
-$result = pg_query($conn, "SELECT array_to_json FROM gene where symbol= 'TP53' and ncbitaxid = '9606'"); 
+$result = pg_query($conn, "SELECT * FROM gene where symbol= 'TP53' and ncbitaxid = '9606'"); 
     // 2. 데이터베이스로부터 반환된 데이터를
     // 객체 형태로 가공함
 if (!$result){
@@ -20,7 +15,17 @@ if (!$result){
 $data = array();
 while ($row = pg_fetch_row($result)) {
     
-    $data[] = $row;
+    $data[] = array('geneid'=> $row[0]);
+    $data[] = array('entrezid'=> $row[1]);
+    $data[] = array('a_ensemblid'=> $row[2]);
+    $data[] = array('a_biogridid'=> $row[3]);
+    $data[] = array('a_pharmgkbid'=> $row[4]);
+    $data[] = array('symbol'=> $row[5]);
+    $data[] = array('a_synonym'=> $row[6]);
+    $data[] = array('ncbitaxid'=> $row[7]);
+    $data[] = array('a_uniprotid'=> $row[8]);
+    $data[] = array('a_ecnumber'=> $row[9]);
+    $data[] = array('a_goid'=> $row[10]);
 }
 
 echo json_encode($data);
